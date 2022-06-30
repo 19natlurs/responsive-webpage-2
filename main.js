368efacb5a5a913bf36906c8beee6ba0d111b8e6
19natlurs <mrnatlurs@gmail.com>
2022-06-30 22:53:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// const menu = document.querySelector('#happen');
+
+
+// const timeline = gsap.timeline();
+// timeline
+//     .to('#side', { translateX: '-100%' })
+//     .to('#side', { display: 'none' })
+//     .to('.overall', { display: 'block' });
+
+// menu.addEventListener('click', () => {
+
+//     console.log('happen');
+//     if (timeline.reversed() && !timeline.isActive()) {
+//         timeline.play()
+//     }
+
+//     if (!timeline.reversed() && !timeline.isActive()) {
+//         timeline.reverse()
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const timeline = gsap.timeline();
+// timeline
+//     .to('.side-menu', 1, { width: -5, opacity: 1 })
+//     .to('.main-menu', 1, { width: 10000 });
+
+
+
+
+
+
+// menu.addEventListener('click', () => {
+//     console.log('menu');
+//     if (timeline.reversed() && !timeline.isActive()) {
+//         timeline.play();
+
+//     }
+
+//     if (!timeline.reversed() && !timeline.isActive()) {
+//         timeline.reverse();
+//         var tl = gsap.timeline();
+//         tl
+//             .fromTo('ul li', 1, { x: 100, opacity: 0, }, { x: 0, opacity: 1, stagger: 1, })
+
+//     }
+// })
